b95b9284c15b0b844db6f44001abada348d67bfa
Flashokiller <75789819+flaahokiller@users.noreply.github.com>
2021-04-17 09:35:05 +0530

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -8,18 +8,8 @@ echo -e "Installing Google Repo"
 mkdir ~/bin && PATH=~/bin:$PATH && curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo && chmod a+x ~/bin/repo 
 
 echo -e "Installing tools"
-apt install \
-    adb autoconf automake axel bc bison build-essential \
-    ccache clang cmake expat fastboot flex g++ \
-    g++-multilib gawk gcc gcc-multilib git gnupg gperf \
-    htop imagemagick lib32ncurses5-dev lib32z1-dev libtinfo5 libc6-dev libcap-dev \
-    libexpat1-dev libgmp-dev '^liblz4-.*' '^liblzma.*' libmpc-dev libmpfr-dev libncurses5-dev \
-    libsdl1.2-dev libssl-dev libtool libxml2 libxml2-utils '^lzma.*' lzop \
-    maven ncftp ncurses-dev patch patchelf pkg-config pngcrush \
-    pngquant python2.7 python-all-dev re2c schedtool squashfs-tools subversion \
-    texinfo unzip w3m xsltproc zip zlib1g-dev lzip \
-    libxml-simple-perl apt-utils -y \
-
+sudo apt-get install  adb autoconf automake axel bc bison build-essential \ccache clang cmake expat fastboot flex g++ g++-multilib gawk gcc gcc-multilib git gnupg gperf  htop imagemagick lib32ncurses5-dev lib32z1-dev libtinfo5 libc6-dev libcap-dev libexpat1-dev libgmp-dev '^liblz4-.*' '^liblzma.*' libmpc-dev libmpfr-dev libncurses5-dev   libsdl1.2-dev libssl-dev libtool libxml2 libxml2-utils '^lzma.*' lzop maven ncftp ncurses-dev patch patchelf pkg-config pngcrush     pngquant python2.7 python  python-all-dev re2c schedtool squashfs-tools subversion texinfo unzip w3m xsltproc zip zlib1g-dev lzip libxml-simple-perl apt-utils
+-y
 echo "Repo sync"
 repo init -u https://github.com/PixelExperience/manifest -b eleven
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
