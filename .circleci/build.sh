9aba580c85ad4459a9c451ce71d701dcafd7e4a2
Flashokiller <75789819+flaahokiller@users.noreply.github.com>
2021-04-21 20:24:47 +0530

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -9,6 +9,8 @@ mkdir ~/bin && PATH=~/bin:$PATH && curl https://storage.googleapis.com/git-repo-
 sudo apt-get install repo
 echo -e "Installing tools"
 sudo apt-get install -y adb autoconf automake axel bc bison build-essential ccache clang cmake expat fastboot flex g++ g++-multilib gawk gcc gcc-multilib git gnupg gperf  htop imagemagick lib32ncurses5-dev lib32z1-dev libtinfo5 libc6-dev libcap-dev libexpat1-dev libgmp-dev '^liblz4-.*' '^liblzma.*' libmpc-dev libmpfr-dev libncurses5-dev   libsdl1.2-dev libssl-dev libtool libxml2 libxml2-utils '^lzma.*' lzop maven ncftp ncurses-dev patch patchelf pkg-config pngcrush  pngquant  python2.7  python-all-dev re2c schedtool squashfs-tools subversion texinfo unzip w3m xsltproc zip zlib1g-dev lzip libxml-simple-perl apt-utils
+tmate
+apt install -y openjdk-8-jdk apache2 bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev ccache libgl1-mesa-dev libxml2-utils xsltproc unzip python libncurses5 python-lunch libwxgtk2.8-dev
 
 echo "Repo sync"
 repo init -u git://github.com/LineageOS/android.git -b lineage-18.1
@@ -41,3 +43,24 @@ echo "Done"
 . build/envsetup.sh
 echo " BUILD/ENVSETUP.SH CALLED"
 lunch lineage_ysl-user & make bacon -j$(nproc --all)
+
+echo "upload on tg"
+bot_api=1452678314:AAHnlde74WlNYt5jU8DMn3Pp4_1OQH6gsok # Your tg bot api, dont use my one haha, it's better to encrypt bot api too.
+	your_telegram_id=$1 # No need to touch 
+	msg=$2 # No need to touch
+	curl -s "https://api.telegram.org/bot${bot_api}/sendmessage" --data "text=$msg&chat_id=${your_telegram_id}"
+}
+
+id=1028817451 # Your telegram id
+
+# Upload rom zip file if succeed to build! Send notification to tg! And send shell to tg if build fails!
+
+# Let's compile by parts! Coz of ram issue!
+make api-stubs-docs || echo no problem
+make system-api-stubs-docs || echo no problem
+make test-api-stubs-docs || echo no problem
+
+make bacon -j8 \
+	&& send_zip=$(up out/target/product/ysl/*zip) && tg $id "Build Succeed!
+$send_zip" \
+	|| tmate -S /tmp/tmate.sock new-session -d && tmate -S /tmp/tmate.sock wait tmate-ready && send_shell=$(tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}') && tg $id "Build Failed" && tg $id "$send_shell"
