5cca71f25c72769633693a6b1bfaffad419e5a3f
Flashokiller <75789819+flaahokiller@users.noreply.github.com>
2021-04-21 19:11:02 +0530

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -19,6 +19,22 @@ git clone --depth=1 https://github.com/flaahokiller/android_device_xiaomi_ysl de
 git clone --depth=1 1https://github.com/ItsVixano/android_vendor_xiaomi_ysl vendor/xiaomi/ysl
 git clone --depth=1 https://github.com/flaahokiller/flasho_Ysl -b MaD kernel/xiaomi/ysl
 
+echo "-------------- Installing scripts----------------"
+git clone https://github.com/dustxyz/scripts
+cd scripts
+./setup/android_build_env.sh
+cd ..
+
+echo " -----------Cloning Hals--------------"
+rm -rf hardware/qcom-caf/msm8996/media hardware/qcom-caf/msm8996/audio hardware/qcom-caf/msm8996/display && git clone --single-branch https://github.com/Jabiyeff/android_hardware_qcom_media hardware/qcom-caf/msm8996/media && git clone --single-branch https://github.com/Jabiyeff/android_hardware_qcom_display hardware/qcom-caf/msm8996/display &&  git clone https://github.com/LineageOS/android_hardware_qcom_audio --single-branch -b lineage-18.1-caf-msm8996 hardware/qcom-caf/msm8996/audio
+
+echo "fixing qti"
+cd device/xiaomi/ysl
+rm -rf overlay/packages/services/Telephony/res/xml/telephony_injection.xml
+cd ../../.
+cd vendor
+rm -rf msm8953-common/proprietary/system_ext/framework/qti-telephony-common.jar
+cd ..
 echo "Done"
 
 . build/envsetup.sh
