2e58f1359ec391033625c61c48db9dfd0e8263ac
Flashokiller <75789819+flaahokiller@users.noreply.github.com>
2021-04-22 09:20:11 +0530

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -12,7 +12,7 @@ sudo apt-get install -y adb autoconf automake axel bc bison build-essential ccac
 apt install -y openjdk-8-jdk apache2 bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev ccache libgl1-mesa-dev libxml2-utils xsltproc unzip python libncurses5 python-lunch libwxgtk2.8-dev
 
 echo "Repo sync"
-repo init -u git://github.com/LineageOS/android.git -b lineage-18.1
+repo init -u https://github.com/PotatoProject/manifest -b dumaloo-release
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 cls
 echo "Cloning dependencies"
@@ -42,5 +42,4 @@ echo "Done"
 . build/envsetup.sh
 echo " BUILD/ENVSETUP.SH CALLED"
 lunch lineage_ysl-userdebug & make bacon -j$(nproc --all)
-cd out/target/product/ysl
 curl --upload-file $1 https://transfer.sh/$(basename $1); echo out/target/product/ysl/*zip
